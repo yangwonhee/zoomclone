a48ff1930b36018e62c889acff219a8a69f43d3a
yangwonhee <yangwonhee@kakao.com>
2022-02-12 13:10:33 +0900

    #0.3 Fronted Setup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ import express, { application } from "express";
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
 const handleListen = () => console.log("Listening on http://localhost:3000");
-
 app.listen(3000);
