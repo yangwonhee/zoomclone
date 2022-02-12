797b1d7b6bf3ad017db4617199ae87a8bb49eea1
yangwonhee <yangwonhee@kakao.com>
2022-02-12 15:55:22 +0900

    #2.1 Installing SocketIO

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -11,16 +11,20 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log("Listening on http://localhost:3000");
 
-// ws server on http server
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
+const io = SocketIO(httpServer);
+
+io.on("connection", (socket) => {
+  console.log(socket);
+});
+
+/*
 const wss = new WebSocket.Server({ server });
 
 function onSocketClose() {
   console.log("Disconnected from the browser");
 }
-
 const sockets = [];
-
 wss.on("connection", (socket) => {
   sockets.push(socket);
   socket["nickname"] = "anonymous";
@@ -39,5 +43,6 @@ wss.on("connection", (socket) => {
     }
   });
 });
+*/
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
